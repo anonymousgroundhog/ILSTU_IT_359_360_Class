77e4bf6f6fcf9d8e23cc5fe6e4797791059d5fbf
Anon <11840058+anonymousgroundhog@users.noreply.github.com>
2026-03-01 17:27:33 -0600

    Create setup.sh

---
+++ Exam_Files/Midterm/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git clone https://github.com/volatilityfoundation/volatility3.git
+cd volatility3/
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt update --fix-missing
+sudo apt install python3.12-venv
+python3 -m venv venv && . venv/bin/activate
+pip install -e ".[dev]"
+cd volatility3/symbols
+wget https://downloads.volatilityfoundation.org/volatility3/symbols/windows.zip
+unzip windows.zip
+wget https://downloads.volatilityfoundation.org/volatility3/symbols/linux.zip
+unzip linux.zip
